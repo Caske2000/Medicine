199b03c32851a046f7d93ee4405ca58c4147c5b9
Caske <vdlucas18@gmail.com>
2015-04-25 21:08:43 +0200

    Tested Collisions and Bounding

--- src/java/com/caske2000/medicine/blocks/HealthBlock.java
+++ src/java/com/caske2000/medicine/blocks/HealthBlock.java
@@ -1,8 +1,34 @@
 package com.caske2000.medicine.blocks;
 
+import java.util.List;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+
 public class HealthBlock extends BlockMod {
 
 	public HealthBlock() {
 		setUnlocalizedName("healthBlock_block");
+	}
+
+	public boolean isOpaqueCube() {
+		return false;
+	}
+
+	public boolean isFullCube() {
+		return false;
+	}
+
+	public void addCollisionBoxesToList(World worldIn, BlockPos pos, IBlockState state, AxisAlignedBB mask, List list, Entity collidingEntity) {
+		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.2F, 1.0F);
+		super.addCollisionBoxesToList(worldIn, pos, state, mask, list, collidingEntity);
+		this.setBlockBounds(0.0F, 0.2F, 0.0F, 0.5F, 1.0F, 0.5F);
+	}
+
+	public void setBlockBoundsForItemRender() {
+		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 0.94F, 1.0F);
 	}
 }
